3408493b88ee95ef191024adb0b049250297d7ee
yyhser <yyhser@126.com>
2021-04-19 15:29:32 +0800

    4.19

---
+++ src/pages/login/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import card from "@/components/card";
+export default {
+  data() {
+    return {
+      columns: ["杭州", "宁波", "温州", "嘉兴", "湖州"],
+      show: false,
+    };
+  },
+  components: {
+    card,
+  },
+  methods: {
+    onChange(picker, value, index) {},
+    bindViewTap() {
+      const url = "../logs/main";
+      if (mpvuePlatform === "wx") {
+        mpvue.switchTab({ url });
+      } else {
+        mpvue.navigateTo({ url });
+      }
+    },
+    clickHandle(ev) {
+      console.log("clickHandle:", ev);
+      // throw {message: 'custom test'}
+    },
+  },
+
+  created() {
+    // let app = getApp()
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
